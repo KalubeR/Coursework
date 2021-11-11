c64dc2894b7f14a918506c147b28b651ad0419e6
KalubeR <kalin.erenditzov@gmail.com>
2021-11-11 12:52:39 +0000

    Added check for no arguments provided

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,6 +7,10 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+	if(args.length == 0)
+	{
+		throw new Exception("No input argument has been provided!");
+	}
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
