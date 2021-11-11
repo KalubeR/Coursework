184ff88518b64986820d07052316e7178f095643
KalubeR <kalin.erenditzov@gmail.com>
2021-11-11 13:09:48 +0000

    Added error handling

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,26 +7,31 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-	if(args.length == 0)
+	try
 	{
-		throw new Exception("No input argument has been provided!");
+		if(args.length == 0)
+		{
+			throw new Exception("No input argument has been provided!");
+		}
+		Arg1 = Integer.parseInt(args[0]);
+		char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		int rem, num;
+		num = Arg1;
+		String hexadecimal="";
+		System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+		while(num != 0)
+		{
+			rem=num%16;
+			hexadecimal= ch[rem] + hexadecimal;
+			num= num/16;
+		}
+
+		System.out.println("Hexadecimal representation is: " + hexadecimal);
 	}
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+	catch(Exception e)
+	{
+		System.out.println(e.getMessage());
+	}
     }
 }
-
