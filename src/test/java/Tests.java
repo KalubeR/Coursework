29f027da8f36e9b914f849f12e3ce746be890ab6
KalubeR <kalin.erenditzov@gmail.com>
2021-11-14 00:46:33 +0200

    Added appropriate tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -4,15 +4,41 @@ import org.junit.Test;
 public class Tests {
 
     @Test
-    public void testStringInput(){
-        String input = "Ok";
+    ///Test the normal flow of the program
+    public void testConvertToHex()
+    {
+        String input = "555";
+        Assert.assertEquals("22B", Dec2Hex.convertToHex(new String[]{input}));
+    }
+
+    @Test
+    ///Test the program with String as input
+    public void testInputWithString()
+    {
+        String input = "text";
         Assert.assertEquals("Argument not an integer", Dec2Hex.convertToHex(new String[]{input}));
     }
 
     @Test
-    ///Test the program with normal arguments
-    public void testConvertToHex(){
-        String input = "2000";
-        Assert.assertEquals("7D0", Dec2Hex.convertToHex(new String[]{input}));
+    //Test the program without any input
+    public void testWithoutInput()
+    {
+        Assert.assertEquals("No input argument has been provided!", Dec2Hex.convertToHex(new String[]{}));
+    }
+
+    @Test
+    ///Test the program with one normal input
+    public void testConvertToHexWithAnotherInput()
+    {
+        String input = "10000";
+        Assert.assertEquals("2710", Dec2Hex.convertToHex(new String[]{input}));
+    }
+
+    @Test
+    ///Test with a negative number as input
+    public void testWithNegativeInput()
+    {
+        String input = "-10";
+        Assert.assertEquals("Only positive numbers are allowed!", Dec2Hex.convertToHex(new String[]{input}));
     }
 }
